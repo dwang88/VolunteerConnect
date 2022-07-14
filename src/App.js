b8817763e4847fe5e7be00d312612b9a6b5d35ff
dwang88 <davidwang1089@gmail.com>
2022-07-13 23:11:28 -0700

    add firestore

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Card from './components/Card';
+import firebase from './firebase';
 
 export default function App() {
   return (
---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAdsHHBYQ0ak5HcDImCNrH7gOX5eFHdYRA",
+  authDomain: "volunteerconnect-ee780.firebaseapp.com",
+  projectId: "volunteerconnect-ee780",
+  storageBucket: "volunteerconnect-ee780.appspot.com",
+  messagingSenderId: "3851916659",
+  appId: "1:3851916659:web:4c72f5cb3047ce70f22106",
+  measurementId: "G-KTCB3DFYVN"
+};
+
+const app = initializeApp(firebaseConfig);
