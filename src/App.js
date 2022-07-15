ff0c7c837e4b791c7380ef02817ba86cb11a13c4
dwang88 <davidwang1089@gmail.com>
2022-07-14 21:34:53 -0700

    fix routes & card display

--- src/App.js
+++ src/App.js
@@ -20,43 +20,6 @@ export default function App() {
           <Route path="/signup" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
-    <Card
-    component="img" 
-    height="365" 
-    image='https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png'
-    alt="image" 
-    title="smth" 
-    body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-    button="Learn More">
-    </Card>
-    <Card
-    component="img" 
-    height="365" 
-    image='https://images-na.ssl-images-amazon.com/images/G/01/gc/designs/livepreview/amazon_a_black_noto_email_v2016_us-main._CB624175556_.png'
-    alt="image" 
-    title="Aip" 
-    body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-    button="Learn More">
-    </Card>
-    <Card
-    component="img" 
-    height="365" 
-    image='https://cdn.shopify.com/app-store/listing_images/21d07b9a03ab6e538a053381def7b15d/icon/CJnzrtj0lu8CEAE=.jpg'
-    alt="image" 
-    title="Facebook Internship" 
-    body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-    button="Learn More">
-    </Card>
-    <Card
-    component="img" 
-    height="365" 
-    image='https://cdn.shopify.com/app-store/listing_images/21d07b9a03ab6e538a053381def7b15d/icon/CJnzrtj0lu8CEAE=.jpg'
-    alt="image" 
-    title="Facebook Internship" 
-    body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-    button="Learn More"
-    className="card5">
-    </Card>
     </div>
   );
 }
--- src/navbar.js
+++ src/navbar.js
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
+import longlogo from '../src/logolong.png';
 
 export default function Navbar() {
     return (
         <nav className="nav">
-            <Link to="/" className="site-title">Volunteer Connect</Link>
+            <Link to="/" className="site-title"><img src={longlogo} style={{height: "4rem", marginTop: "4px"}}/></Link>
             <ul className="pages">
                 <li><CustomLink to="/volunteer">Volunteer</CustomLink></li>
                 <li><CustomLink to="/internships">Internships</CustomLink></li>
